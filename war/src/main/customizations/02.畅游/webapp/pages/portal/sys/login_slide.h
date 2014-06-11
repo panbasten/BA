f4e2579fb1238617993d3f1ab19826d75a1a6202
Peter Pan <panbasten@126.com>
2014-06-11 20:20:58 +0800

    aa

---
+++ war/src/main/customizations/02.畅游/webapp/pages/portal/sys/login_slide.h
@@ -0,0 +1,34 @@
+<fly:composition freeLayout="N">
+
+	<div class="slide" style="background: url('resources/images/login/section/architecture.png') no-repeat scroll 0 0 transparent;">
+		<p style="font-size: 36px;line-height:1em;margin:40px 0 0 40px;color:#D3D4D5;font-weight:bold;text-shadow:1px 1px 1px #C89F40;">
+			<font>畅</font><br/>
+			<font>游</font><br/>
+			<font style="font-size:26px;">DA</font>
+		</p>
+	</div>
+	<div class="slide" style="background: url('resources/images/login/section/mascot.png') no-repeat scroll 0 0 transparent;">
+		<p class="slide-p" style="margin: 25px 0 0 170px;">您的企业是否面临这些问题？</p>
+		<ul class="slide-ul" style="margin:15px 0 0 150px;color:#9CA839;">
+			<li>数据量庞大，并且格式复杂</li>
+			<li>数据来源众多，并且质量差</li>
+			<li>数据实时掌控困难</li>
+			<li>报表不能准确反映业务情况</li>
+			<li>移动办公应用逐步展开</li>
+		</ul>
+	</div>
+	<div class="slide" style="background: url('resources/images/login/section/mascot.png') no-repeat scroll 0 0 transparent;">
+		<p class="slide-p" style="margin: 25px 0 0 170px;">您是否期望得到这样的答案？</p>
+		<ul class="slide-ul" style="margin:15px 0 0 150px;color:#4066C4;">
+			<li>全面、一致、干净、可靠的数据</li>
+			<li>准实时大数据量企业数据监控</li>
+			<li>纷繁准确的企业报表</li>
+			<li>现代化移动办公</li>
+			<li>自助式BA业务定制能力</li>
+		</ul>
+	</div>
+	<div class="slide">
+		<p class="slide-single-p" style="margin:65px 0 0 40px;">数据分析平台全部帮您实现！</p>
+	</div>
+
+</fly:composition>
